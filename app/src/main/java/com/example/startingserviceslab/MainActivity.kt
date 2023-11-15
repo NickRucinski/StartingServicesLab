3eafac80a86f081f326460aa71db75cc838a28a6
Nick R <tur45021@temple.edu>
2023-11-15 15:23:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/startingserviceslab/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.startingserviceslab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+Create an application that contains one Activity and one Service. The service will be Started, not Bound.
+On the Activity's layout, add an EditText and a Button
+In the service class, implement countdown timer logic using Coroutines that is able to count down from a user provided value.
+The user should enter an integer value in the activity's EditText. When the user then clicks the button, the service should be Started with startService(Intent)
+The service should be given the value that was entered by the user in the EditText and once received a countdown should being (you can print this to the Log since we don't have a Binder or Handler)
+Investigate and implement this capability. Remember that Intents are messaging objects.
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
