08ca0a9753c29e30a7ca5d9202dc935f8803dc54
Nick R <tur45021@temple.edu>
2023-11-15 15:46:00 -0500

    created a function that runs the timer in a coroutine

--- app/src/main/java/com/example/startingserviceslab/TimerService.kt
+++ app/src/main/java/com/example/startingserviceslab/TimerService.kt
@@ -4,6 +4,11 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class TimerService : Service(){
     override fun onBind(p0: Intent?): IBinder? {
@@ -11,13 +16,21 @@ class TimerService : Service(){
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-
-
+        if(intent != null){
+            runTimer(intent.getIntExtra("StartNumber", 10))
+        }
         return super.onStartCommand(intent, flags, startId)
     }
 
     fun runTimer(startNum: Int){
-
+        CoroutineScope(Dispatchers.Default).launch {
+            repeat(startNum){
+                (startNum-it).toString().run{
+                    Log.d("Countdown", this)
+                }
+                delay(1000)
+            }
+        }
     }
 
 
